aba9af43118391a3997f1575af96bb07cd9b5fd8
Mark Zemlianskyi <mark080501@gmail.com>
2023-03-19 22:17:34 +0200

    header hero

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,5 +1,5 @@
-module.exports = require('./utils/postcss-preset')({
+module.exports = require("./utils/postcss-preset")({
   plugins: [
-    process.env.NODE_ENV === 'production' && require('cssnano')()
-  ].filter(Boolean)
+    process.env.NODE_ENV === "production" && require("cssnano")(),
+  ].filter(Boolean),
 });
