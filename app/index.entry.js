c448df52c830e11b159843ff4f207774f7bc153f
Mark Zemlianskyi <mark080501@gmail.com>
2023-03-23 18:38:14 +0200

    sections

--- app/index.entry.js
+++ app/index.entry.js
@@ -1,14 +1,12 @@
-import 'reset-css';
-import './exterrnal-libs/layout/index.css'
-import fallbackStylesheetUrl from '!!file-loader!./exterrnal-libs/layout/layout-without-css-vars.css';
-import { supportsCssVars, createLink } from './exterrnal-libs/layout/utils';
-import './index.entry.pcss';
-import './exterrnal-libs/breakpoints/index.css';
+import "reset-css";
+import "./exterrnal-libs/layout/index.css";
+import fallbackStylesheetUrl from "!!file-loader!./exterrnal-libs/layout/layout-without-css-vars.css";
+import { supportsCssVars, createLink } from "./exterrnal-libs/layout/utils";
+import "./index.entry.pcss";
+import "./exterrnal-libs/breakpoints/index.css";
 
 if (!supportsCssVars()) {
   createLink(fallbackStylesheetUrl);
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-
-});
+document.addEventListener("DOMContentLoaded", () => {});
